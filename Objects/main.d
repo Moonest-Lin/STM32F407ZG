18ad24d68b5473ab4af68d7a4e7678aa32a2ef30
Moonest <Moonest@tutanota.com>
2024-02-27 10:02:51 +0800

    First release

---
+++ Objects/main.d
@@ -0,0 +1,8 @@
+.\objects\main.o: User\Src\main.c
+.\objects\main.o: .\CMSIS\stm32f4xx.h
+.\objects\main.o: .\CMSIS\Include\core_cm4.h
+.\objects\main.o: C:\Software\MDK\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: .\CMSIS\Include\core_cmInstr.h
+.\objects\main.o: .\CMSIS\Include\core_cmFunc.h
+.\objects\main.o: .\CMSIS\Include\core_cm4_simd.h
+.\objects\main.o: .\CMSIS\system_stm32f4xx.h
